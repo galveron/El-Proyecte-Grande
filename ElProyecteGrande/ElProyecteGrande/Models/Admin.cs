59377ec70e6b01ffc12be38ae15a73f2de5b19d6
Kbence9 <bence.kristian@gmail.com>
2024-01-23 11:58:52 +0100

    feat - add User, Admin and Role models

---
+++ ElProyecteGrande/ElProyecteGrande/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace ElProyecteGrandeReact.Models;
+
+public class Admin : User
+{
+    public Admin(string username, string password) : base(username, password, Role.Admin)
+    {
+        
+    }
+}
---
+++ ElProyecteGrande/ElProyecteGrande/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ElProyecteGrandeReact.Models;
+
+public abstract class User
+{
+    public readonly int Id;
+    public readonly string Username;
+    private string _password;
+    public readonly Role Role;
+
+    public User(string username, string password, Role role)
+    {
+        Username = username;
+        _password = password;
+        Role = role;
+    }
+}
