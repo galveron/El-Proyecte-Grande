6e9a3217ad609b103b66d441093ca74de18debc2
Kbence9 <bence.kristian@gmail.com>
2024-01-23 11:59:44 +0100

    feat - add UserRepository.cs

---
+++ ElProyecteGrande/ElProyecteGrande/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using ElProyecteGrandeReact.Models;
+
+namespace ElProyecteGrandeReact.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private List<User> _users = new();
+    
+    public User GetUser(string username)
+    {
+        return _users.Single(user => user.Username == username);
+    }
+
+    public IEnumerable<User> GetUsers()
+    {
+        return _users;
+    }
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public void DeleteUser(string username)
+    {
+        var itemToRemove = _users.First(user => user.Username == username);
+        _users.Remove(itemToRemove);
+    }
+    
+}
