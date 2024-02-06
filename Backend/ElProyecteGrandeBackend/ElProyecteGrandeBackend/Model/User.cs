d17fcf2ef2c19c474d5bf249846e2b59466c7304
Atromat <atromatok@gmail.com>
2024-02-06 13:57:12 +0100

    change - Favourites and Cart to ICollection

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/User.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/User.cs
@@ -9,6 +9,6 @@ public class User
     public string Email { get; init; }
     public string PhoneNumber { get; init; }
     public Company? Company { get; init; }
-    public List<Product> Favourites { get; } = new List<Product>();
-    public List<Product> Cart { get; } = new List<Product>();
+    public ICollection<Product> Favourites { get; } = new List<Product>();
+    public ICollection<Product> Cart { get; } = new List<Product>();
 }
