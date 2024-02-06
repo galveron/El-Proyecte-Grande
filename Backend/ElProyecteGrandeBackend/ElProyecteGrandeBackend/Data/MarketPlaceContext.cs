4034137d2482e42d5bd0d237023dd5a35d34e76d
Kbence9 <bence.kristian@gmail.com>
2024-02-06 14:04:32 +0100

    in progress - create migrations

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Data/MarketPlaceContext.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Data/MarketPlaceContext.cs
@@ -5,9 +5,9 @@ namespace ElProyecteGrandeBackend.Data;
 
 public class MarketPlaceContext : DbContext
 {
-    public DbSet<User> Users { get; init; }
-    public DbSet<Product> Products { get; init; }
-    public DbSet<Order> Orders { get; init; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -15,5 +15,24 @@ public class MarketPlaceContext : DbContext
             @$"Server={Environment.GetEnvironmentVariable("SERVER")},{Environment.GetEnvironmentVariable("PORT")};
 Database={Environment.GetEnvironmentVariable("DATABASE")};User Id={Environment.GetEnvironmentVariable("USERID")};
 Password={Environment.GetEnvironmentVariable("PASSWORD")};Encrypt={Environment.GetEnvironmentVariable("ENCRYPT")};");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>()
+            .HasIndex(u => u.Id)
+            .IsUnique();
+        builder.Entity<Company>(company => company.HasNoKey());
+        builder.Entity<User>().Ignore("Company");
+        builder.Entity<Product>()
+            .HasIndex(p => p.Id)
+            .IsUnique();
+        builder.Entity<Product>()
+            .HasOne<User>()
+            .WithOne()
+            .HasForeignKey("User");
+        builder.Entity<Order>()
+            .HasIndex(o => o.Id)
+            .IsUnique();
     }
 }
--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/Product.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/Product.cs
@@ -3,16 +3,15 @@ namespace ElProyecteGrandeBackend.Model;
 public class Product
 {
     public int Id { get; init; }
-    public int UserId { get; init; }
+    public int User { get; init; }
     public decimal Price { get; init; }
     public string Details { get; init; }
     public int Quantity { get; init; }
-
-    public Product(int userId, decimal price, string details, int quantity)
+/*
+    public Product( decimal price, string details, int quantity)
     {
-        UserId = userId;
         Price = price;
         Details = details;
         Quantity = quantity;
-    }
+    }*/
 }
