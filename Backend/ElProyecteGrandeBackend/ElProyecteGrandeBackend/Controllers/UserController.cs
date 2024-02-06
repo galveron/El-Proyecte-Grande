8e11d829a9924934a2d1fe484c287b3eae30d0c2
Atromat <atromatok@gmail.com>
2024-02-06 20:34:55 +0100

    feat - add GetUser, AddFavourite and (only for testing) AddProduct to UserController

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Controllers/UserController.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using ElProyecteGrandeBackend.Model;
 using ElProyecteGrandeBackend.Services.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +9,81 @@ namespace ElProyecteGrandeBackend.Controllers;
 public class UserController : ControllerBase
 {
     private readonly ILogger<UserController> _logger;
-    private IUserRepository _userRepository;
-    private IOrderRepository _orderRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IOrderRepository _orderRepository;
+    private readonly IProductRepository _productRepository;
 
-    public UserController(ILogger<UserController> logger, IUserRepository userRepository, IOrderRepository orderRepository)
+    public UserController(
+        ILogger<UserController> logger, 
+        IUserRepository userRepository, 
+        IOrderRepository orderRepository,
+        IProductRepository productRepository)
     {
         _logger = logger;
         _userRepository = userRepository;
         _orderRepository = orderRepository;
+        _productRepository = productRepository;
+    }
+
+    [HttpGet("GetUser")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        try
+        {
+            var user = _userRepository.GetUser(id);
+            
+            if (user == null)
+            {
+                return NotFound("User was not found.");
+            }
+            
+            return Ok(user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500);
+        }
+    }
+    
+    [HttpGet("AddFavourite")]
+    public async Task<ActionResult> AddFavourite(int UserId, int ProductId)
+    {
+        try
+        {
+            _userRepository.AddFavourite(_userRepository.GetUser(UserId), _productRepository.GetProduct(ProductId));
+            
+            return Ok("Done.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500);
+        }
+    }
+    
+    //ONLY FOR TEST
+    [HttpGet("AddProduct")]
+    public async Task<ActionResult> AddProduct()
+    {
+        try
+        {
+            _productRepository.AddProduct(
+                new Product
+                {
+                    Details = "Hát persze, hogy minőségi",
+                    Price = 999,
+                    Quantity = 666,
+                    Seller = _userRepository.GetUser(2)
+                }
+                );
+            
+            return Ok("Done.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500);
+        }
     }
 }
