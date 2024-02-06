f22464682ecf24d9911ab43bfe710069e94d39f7
Atromat <atromatok@gmail.com>
2024-02-06 13:55:25 +0100

    change - add initial value for Favourites and Cart in User

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/User.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/User.cs
@@ -9,6 +9,6 @@ public class User
     public string Email { get; init; }
     public string PhoneNumber { get; init; }
     public Company? Company { get; init; }
-    public List<Product> Favourites { get; init; }
-    public List<Product> Cart { get; init; }
+    public List<Product> Favourites { get; } = new List<Product>();
+    public List<Product> Cart { get; } = new List<Product>();
 }
