fdc5c6d4f2cbb9365b9f40ac7970ca8b1436f255
Atromat <atromatok@gmail.com>
2024-02-06 13:35:15 +0100

    feat - add AddFavourite and AddToCart to IUserRepository

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Services/Repositories/IUserRepository.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Services/Repositories/IUserRepository.cs
@@ -7,6 +7,8 @@ public interface IUserRepository
     IEnumerable<User> GetAllUsers();
     User? GetUser(int id);
     void AddUser(User user);
-    void DeleteUser(int id);
+    void DeleteUser(User user);
     void UpdateUser(User user);
+    void AddFavourite(User user, Product product);
+    void AddToCart(User user, Product product);
 }
