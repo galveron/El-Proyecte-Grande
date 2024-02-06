713537a738e18e5b6ba41981fdce297caf26be67
Atromat <atromatok@gmail.com>
2024-02-06 20:42:39 +0100

    change - the Products in Order to ICollection

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/Order.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/Order.cs
@@ -5,7 +5,7 @@ public class Order
     public int Id { get; init; }
     public int UserId { get; init; }
     public DateTime Date { get; init; }
-    public List<Product> Products { get; init; }
+    public ICollection<Product> Products { get; init; }
     public decimal PriceToPay { get; init; }
 
     public Order(int userId, DateTime date)
