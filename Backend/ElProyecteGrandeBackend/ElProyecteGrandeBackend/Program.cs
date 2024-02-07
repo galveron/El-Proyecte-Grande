858fe37961f9b43c8810364c1781e3ac3391740b
Atromat <atromatok@gmail.com>
2024-02-07 10:33:09 +0100

    style - remove unnecessary ";"

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Program.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
