3f531dc00f13c03c37de33a092aa033acaa0c13d
Kbence9 <bence.kristian@gmail.com>
2024-02-08 09:49:03 +0100

    update - refactor order

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/Order.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Model/Order.cs
@@ -7,25 +7,5 @@ public class Order
     public DateTime Date { get; init; }
     public ICollection<Product> Products { get; } = new List<Product>();
     public decimal PriceToPay { get; set; }
-/*
-    public Order(User user, DateTime date)
-    {
-        User = user;
-        Date = date;
-        Products = new List<Product>();
-    }
     
-    public Order(User user, DateTime date, ICollection<Product> products)
-    {
-        User = user;
-        Date = date;
-        Products = products;
-        PriceToPay = products.Sum(p => p.Price * p.Quantity);
-    }
-*/
-    public void AddProduct(Product product)
-    {
-        Products.Add(product);
-        PriceToPay = Products.Sum(p => p.Price * p.Quantity);
-    }
 }
