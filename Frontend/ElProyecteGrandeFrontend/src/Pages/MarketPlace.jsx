225e50328fc2ea14b5192a066239f7361d3d7049
kepelivi <kepelivi7@gmail.com>
2024-02-08 13:59:10 +0100

    feat - add basic design to marketplace

--- Frontend/ElProyecteGrandeFrontend/src/Pages/MarketPlace.jsx
+++ Frontend/ElProyecteGrandeFrontend/src/Pages/MarketPlace.jsx
@@ -5,30 +5,34 @@ function MarketPlace() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    async function FetchProducts() 
-    {
+    async function FetchProducts() {
         const response = await fetch("http://localhost:5036/Product/GetAllProducts");
         return await response.json();
     }
 
     useEffect(() => {
         FetchProducts()
-        .then(products => setProducts(products), setLoading(false));
+            .then(products => setProducts(products), setLoading(false));
 
     }, [])
 
     return (<>
-    {loading ? <h1>Loading...</h1> : 
-    products.map(product => {return (
-    <div className='productCard' key={product.id}>
-        <p>{product.id}</p>
-        <p>{product.seller.name}</p>
-        <p>{product.price}</p>
-        <p>{product.details}</p>
-        <p>{product.quantity}</p>
-    </div>
-    )})
-    }</>
+        {loading ? <h1>Loading...</h1> :
+            <div className='productcard-container'>
+                {products.map(product => {
+                    return (
+                        <div className='productCard' key={product.id}>
+                            <div className='card-overlay'></div>
+                            <p>{product.id}</p>
+                            <p>Seller: {product.seller.name}</p>
+                            <p>Price: {product.price}$</p>
+                            <p>Description: {product.details}</p>
+                            <p>In stock: {product.quantity}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        }</>
     )
 
 }
