264c33ff7b10ad33dabafa3f168f7774490430b7
Veronika Gal <galveron@gmail.com>
2024-02-19 12:09:39 +0100

    feat - add IAuthService and AuthService

---
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Services/Authentication/IAuthService.cs
@@ -0,0 +1,7 @@
+namespace ElProyecteGrandeBackend.Services.Authentication;
+
+public interface IAuthService
+{
+    Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
+    Task<AuthResult> LoginAsync(string email, string password);
+}
