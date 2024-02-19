f388d50271f57c485fee960b3d5c5d2c4221369e
Veronika Gal <galveron@gmail.com>
2024-02-19 12:13:35 +0100

    feat - add necessary lines in Program

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Program.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Program.cs
@@ -2,10 +2,12 @@ using System.Text;
 using System.Text.Json.Serialization;
 using AskMate.Service;
 using ElProyecteGrandeBackend.Data;
+using ElProyecteGrandeBackend.Services.Authentication;
 using ElProyecteGrandeBackend.Services.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var root = Directory.GetCurrentDirectory();
 var dotenv = Path.Combine(root, "connectionString.env");
@@ -20,6 +22,7 @@ var config =
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigureSwagger();
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
@@ -33,15 +36,8 @@ builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
 builder.Services.AddDbContext<MarketPlaceContext>();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "MyAllowSpecificOrigins",
-        policy  =>
-        {
-            policy.WithOrigins("*");
-        });
-});
-
+AddCors();
+AddAuthentication();
 AddIdentity();
 
 var app = builder.Build();
@@ -57,6 +53,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -65,6 +62,81 @@ AddRoles();
 AddAdmin();
 
 app.Run();
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
+}
+
+void AddCors()
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "MyAllowSpecificOrigins",
+            builder  =>
+            {
+                // Allow any header and method for simplicity
+                builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+    });
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var jwtSettings = config.GetSection("jwtSettings");
+            var issuerSignInKey = Environment.GetEnvironmentVariable("ISSUERSIGNINKEY");
+            var validIssuer = Environment.GetEnvironmentVariable("VALIDISSUER");
+            var validAudience = Environment.GetEnvironmentVariable("VALIDAUDIENCE");
+        
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = validIssuer,
+                ValidAudience = validAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuerSignInKey)
+                ),
+            };
+        });
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+}
 
 void AddIdentity()
 {
@@ -97,12 +169,14 @@ void AddRoles()
 
 async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
 {
-    await roleManager.CreateAsync(new IdentityRole("Admin")); //The role string should better be stored as a constant or a value in appsettings
+    var adminRole = Environment.GetEnvironmentVariable("ADMINROLE");
+    await roleManager.CreateAsync(new IdentityRole(adminRole)); 
 }
 
 async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
 {
-    await roleManager.CreateAsync(new IdentityRole("User")); //The role string should better be stored as a constant or a value in appsettings
+    var userRole = Environment.GetEnvironmentVariable("USERROLE");
+    await roleManager.CreateAsync(new IdentityRole(userRole));
 }
 
 void AddAdmin()
@@ -116,10 +190,11 @@ async Task CreateAdminIfNotExists()
     using var scope = app.Services.CreateScope();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
     var adminInDb = await userManager.FindByEmailAsync("admin@admin.hu");
+    var adminPassword = Environment.GetEnvironmentVariable("ADMINPASSWORD");
     if (adminInDb == null)
     {
         var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.hu" };
-        var adminCreated = await userManager.CreateAsync(admin, "admin123");
+        var adminCreated = await userManager.CreateAsync(admin, adminPassword);
 
         if (adminCreated.Succeeded)
         {
