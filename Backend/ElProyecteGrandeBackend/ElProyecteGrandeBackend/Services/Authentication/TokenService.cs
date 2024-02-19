c0be23b7d08e2cc3fc2e4ea90f2e1fefb9a313e3
Veronika Gal <galveron@gmail.com>
2024-02-19 12:10:28 +0100

    feat - add ITokenServices and TokenServices

---
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Services/Authentication/TokenService.cs
@@ -0,0 +1,71 @@
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ElProyecteGrandeBackend.Services.Authentication;
+
+public class TokenService : ITokenService
+{
+    private const int ExpirationMinutes = 30;
+    
+    public string CreateToken(IdentityUser user, string role)
+    {
+        var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
+        var token = CreateJwtToken(
+            CreateClaims(user, role),
+            CreateSigningCredentials(),
+            expiration
+        );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+
+    private JwtSecurityToken CreateJwtToken(List<Claim> claims, SigningCredentials credentials,
+        DateTime expiration) =>
+        new(
+            Environment.GetEnvironmentVariable("VALIDISSUER"),
+            Environment.GetEnvironmentVariable("VALIDAUDIENCE"),
+            claims,
+            expires: expiration,
+            signingCredentials: credentials
+        );
+
+    private List<Claim> CreateClaims(IdentityUser user, string? role)
+    {
+        try
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "TokenForTheApiWithAuth"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+            if (role != null)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("ISSUERSIGNINKEY"))
+            ),
+            SecurityAlgorithms.HmacSha256
+        );
+    }
+}
