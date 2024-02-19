017663a26e520098bec9a5c7d9b61b7338e87426
Veronika Gal <galveron@gmail.com>
2024-02-19 12:07:32 +0100

    feat - add AuthResult in Services

---
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Services/Authentication/AuthResult.cs
@@ -0,0 +1,11 @@
+namespace ElProyecteGrandeBackend.Services.Authentication;
+
+public record AuthResult(
+    bool Success,
+    string Email,
+    string UserName,
+    string Token)
+{
+    //Error code - error message
+    public readonly Dictionary<string, string> ErrorMessages = new();
+}
