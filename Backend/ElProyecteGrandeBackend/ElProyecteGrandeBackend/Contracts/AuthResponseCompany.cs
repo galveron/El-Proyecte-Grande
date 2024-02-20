9098e65afb80eb6f9545fb3a036958d6d35b46c1
Veronika Gal <galveron@gmail.com>
2024-02-20 12:11:06 +0100

    feat - add company requests, response and result

---
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Contracts/AuthResponseCompany.cs
@@ -0,0 +1,3 @@
+namespace ElProyecteGrandeBackend.Contracts;
+
+public record AuthResponseCompany(string Email, string UserName, string companyName, string Token);
---
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeBackend/Contracts/RegistrationResponseCompany.cs
@@ -0,0 +1,6 @@
+namespace ElProyecteGrandeBackend.Services;
+
+public record RegistrationResponseCompany(
+    string Email, 
+    string UserName,
+    string CompanyName);
