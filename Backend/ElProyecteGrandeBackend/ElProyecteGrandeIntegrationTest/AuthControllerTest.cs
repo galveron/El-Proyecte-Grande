aa2ec5753df69f7a828d96c5fe091c8a449958cb
Kbence9 <bence.kristian@gmail.com>
2024-03-05 13:54:20 +0100

    feat - add integration tests for auth controller

--- Backend/ElProyecteGrandeBackend/ElProyecteGrandeIntegrationTest/AuthControllerTest.cs
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeIntegrationTest/AuthControllerTest.cs
@@ -37,4 +37,76 @@ public class AuthControllerTest
         
         Assert.Equal(HttpStatusCode.BadRequest,response.StatusCode );
     }
+
+    [Fact]
+    public async Task UserLoginSuccessfullyTest()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        const string testEmail = "valaki@gm.com";
+        const string testPassword = "Valaki123456";
+        var registrationRequest = new RegistrationRequest(testEmail, "valakik", testPassword);
+        var loginRequest = new AuthRequest(testEmail, testPassword);
+
+        var client = application.CreateClient();
+
+        var registerResponse = await client.PostAsJsonAsync("/Auth/Register", registrationRequest);
+
+        registerResponse.EnsureSuccessStatusCode();
+        
+        var loginResponse = await client.PostAsJsonAsync("/Auth/Login", loginRequest);
+
+        loginResponse.EnsureSuccessStatusCode();
+
+        var authResponse = await loginResponse.Content.ReadFromJsonAsync<AuthResponse>();
+        
+        Assert.Equal(testEmail, authResponse?.Email);
+        Assert.NotNull(authResponse?.Token);
+    }
+    
+    [Fact]
+    public async Task UserLoginWrongCredentialsTest()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        const string testEmail = "valaki@g.com";
+        const string testPassword = "Valaki123456";
+        var registrationRequest = new RegistrationRequest(testEmail, "valaki", testPassword);
+        var loginRequest = new AuthRequest("something", "nothing");
+
+        var client = application.CreateClient();
+
+        var registerResponse = await client.PostAsJsonAsync("/Auth/Register", registrationRequest);
+        var loginResponse = await client.PostAsJsonAsync("/Auth/Login", loginRequest);
+
+        Assert.Equal(HttpStatusCode.BadRequest, loginResponse.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CompanyRegistrationSuccessfullyTest()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        var request = new RegistrationRequestCompany("valaki23@g.com", "valaki23", "Valaki123", "valakiBT", "AB12345");
+
+        var client = application.CreateClient();
+
+        var response = await client.PostAsJsonAsync("/Auth/RegisterCompany", request);
+        
+        response.EnsureSuccessStatusCode();
+        
+        var authResponse = await response.Content.ReadFromJsonAsync<RegistrationResponse>();
+        
+        Assert.Equal("valaki23@g.com", authResponse.Email);
+    }
+
+    [Fact]
+    public async Task CompanyRegistrationWrongCredentialsTest()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        var request = new RegistrationRequestCompany("valaki@g.com", "valaki", "Va", "", "");
+
+        var client = application.CreateClient();
+
+        var response = await client.PostAsJsonAsync("/Auth/RegisterCompany", request);
+        
+        Assert.Equal(HttpStatusCode.BadRequest,response.StatusCode );
+    }
 }
