f656346d49f8b8c82f5616c238c8b9e0e776f90f
kepelivi <kepelivi7@gmail.com>
2024-03-06 13:06:55 +0100

    test - cover product controller with integration tests

---
+++ Backend/ElProyecteGrandeBackend/ElProyecteGrandeIntegrationTest/ProductControllerTest.cs
@@ -0,0 +1,106 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using ElProyecteGrandeBackend.Controllers;
+using ElProyecteGrandeBackend.Data;
+using ElProyecteGrandeBackend.Model;
+using ElProyecteGrandeBackend.Services;
+using ElProyecteGrandeBackend.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace ElProyecteGrandeIntegrationTest;
+
+[Collection("Integration")]
+public class ProductControllerTest
+{
+    [Fact]
+    public async Task GetAllProductsReturnsAllProducts()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        
+        var client = application.CreateClient();
+
+        var response = await client.GetAsync("/Product/GetAllProducts");
+        
+        response.EnsureSuccessStatusCode();
+
+        var authResponse = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+        
+        Assert.NotNull(authResponse);
+    }
+
+    [Fact]
+    public async Task GetProductReturnsExpectedProduct()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        var request = new RegistrationRequestCompany("valaki@g.com", "valaki", "Valaki123456", "ceg", "123");
+        
+        var client = application.CreateClient();
+
+        var regResponse = await client.PostAsJsonAsync("/Auth/RegisterCompany", request);
+
+        regResponse.EnsureSuccessStatusCode();
+        
+        var authResponse = await regResponse.Content.ReadFromJsonAsync<RegistrationResponseCompany>();
+
+        await client.PostAsJsonAsync("/Auth/Login", new { });
+
+        var getUserRes = await client.GetAsync($"/User/GetUser?userName={authResponse.UserName}");
+
+        getUserRes.EnsureSuccessStatusCode();
+
+        var user = await getUserRes.Content.ReadFromJsonAsync<User>();
+        
+        var product = new Product { Name = "term", Seller = user, Price = 123, Details = "rrr", Quantity = 2 };
+
+        var addProductRes = await client.PostAsJsonAsync(
+            $"/Product/AddProduct?userId={user.Id}&name={product.Name}&price={product.Price}&details={product.Details}&quantity={product.Quantity}",
+            new { });
+
+        addProductRes.EnsureSuccessStatusCode();
+
+        var productRes = await client.GetAsync("/Product/GetProduct?id=1");
+
+        productRes.EnsureSuccessStatusCode();
+
+        var response = await productRes.Content.ReadFromJsonAsync<Product>();
+        
+        Assert.Equal("term", response.Name);
+    }
+
+    [Fact]
+    public async Task AddProduct_SuccessfullyAddsProduct()
+    {
+        var application = new MarketPlaceWebApplicationFactory();
+        var request = new RegistrationRequestCompany("valaki@g.com", "valaki", "Valaki123456", "ceg", "123");
+        
+        var client = application.CreateClient();
+
+        var regResponse = await client.PostAsJsonAsync("/Auth/RegisterCompany", request);
+
+        regResponse.EnsureSuccessStatusCode();
+        
+        var authResponse = await regResponse.Content.ReadFromJsonAsync<RegistrationResponseCompany>();
+
+        await client.PostAsJsonAsync("/Auth/Login", new { });
+
+        var getUserRes = await client.GetAsync($"/User/GetUser?userName={authResponse.UserName}");
+
+        getUserRes.EnsureSuccessStatusCode();
+
+        var user = await getUserRes.Content.ReadFromJsonAsync<User>();
+        
+        var product = new Product { Name = "term", Seller = user, Price = 123, Details = "rrr", Quantity = 2 };
+
+        var addProductRes = await client.PostAsJsonAsync(
+            $"/Product/AddProduct?userId={user.Id}&name={product.Name}&price={product.Price}&details={product.Details}&quantity={product.Quantity}",
+            new { });
+
+        addProductRes.EnsureSuccessStatusCode();
+
+        var addedProduct = await addProductRes.Content.ReadFromJsonAsync<Product>();
+        
+        Assert.Equal("term", addedProduct.Name);
+    }
+}
