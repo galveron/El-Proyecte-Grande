6f173f575317838ca1d23801331b7c5a2de4a33c
Veronika Gal <galveron@gmail.com>
2024-03-19 15:05:59 +0100

    feat - handle Admin login at Home page

--- Frontend/ElProyecteGrandeFrontend/src/Pages/Home/Home.jsx
+++ Frontend/ElProyecteGrandeFrontend/src/Pages/Home/Home.jsx
@@ -3,9 +3,10 @@ import './Home.css';
 const Home = ({ userRole }) => (
     <div className="home-page">
         <img src="/background1.jpg" />
-
-        <h2 className='welcome'>Welcome to E-Jungle {userRole}! <br />Feel free to check out our Marketplace and order a nice plant for yourself!</h2>
-
+        {userRole === "Admin" ?
+            <h2 className='welcome'>Hello Admin! <br />Do not forget to check the companies waiting for validation!</h2>
+            : <h2 className='welcome'>Welcome to E-Jungle {userRole}! <br />Feel free to check out our Marketplace and order a nice plant for yourself!</h2>
+        }
     </div>
 )
 
