74496053f011a9057ede74ae45eba8c4549b66f4
Kbence9 <bence.kristian@gmail.com>
2024-03-20 10:30:46 +0100

    update - cart modal

--- Frontend/ElProyecteGrandeFrontend/src/Pages/Layout/Layout.jsx
+++ Frontend/ElProyecteGrandeFrontend/src/Pages/Layout/Layout.jsx
@@ -142,14 +142,18 @@ function Layout({ userRole }) {
 
                 </div>
                 {isShowCart ?
-                    <section className="modal">
+                    <section className="cartModal">
+                        <button className="closeModalButton" onClick={() => setIsShowCart(false)}>Close</button>
                         {cartItems ? cartItems.map((item) => {
-                            return (<div key={item.id}>
+                            return (<div key={item.id} className="cartItem">
                                 <p>Name: {item.product.name}</p>
                                 <p>Quantity: {item.quantity}</p>
                                 <p>Price {item.quantity * item.product.price}</p>
                             </div>)
                         }) : <></>}
+                        <p>Total Price: {cartItems && cartItems.length > 0? 
+                        cartItems.reduce((accumulator, currentValue) => 
+                        (accumulator.product.price * accumulator.quantity) + (currentValue.product.price * currentValue.quantity)) : 0} </p>
                     </section> : <></>}
             </header >
             <Outlet />
