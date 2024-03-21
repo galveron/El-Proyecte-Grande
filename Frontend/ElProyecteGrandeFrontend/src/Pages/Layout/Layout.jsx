25ff249f0c0c884ec52c7439dd5462e71b025e45
Veronika Gal <galveron@gmail.com>
2024-03-22 00:02:35 +0100

    change - put registration and login in modal

--- Frontend/ElProyecteGrandeFrontend/src/Pages/Layout/Layout.jsx
+++ Frontend/ElProyecteGrandeFrontend/src/Pages/Layout/Layout.jsx
@@ -4,6 +4,7 @@ import { notification } from 'antd';
 import CartModal from "./CartModal";
 import { useState } from "react";
 import './Layout.css';
+import { Button, Modal } from 'flowbite-react';
 
 notification.config({
     duration: 2,
@@ -34,9 +35,46 @@ function eraseCookie(name) {
 function Layout({ userRole, user }) {
     let isToken = getCookie('User');
     const navigate = useNavigate();
-
+    const [showRegisterCustomer, setShowRegisterCustomer] = useState(false)
+    const [showRegisterCompany, setShowRegisterCompany] = useState(false)
+    const [showLogin, setShowLogin] = useState(null)
+    const [customerUserName, setCustomerUserName] = useState(null)
+    const [customerEmail, setCustomerEmail] = useState(null)
+    const [customerPassword, setCustomerPassword] = useState(null)
+    const [showRegister, setShowRegister] = useState(false)
+    const [companyUserName, setCompanyUserName] = useState(null)
+    const [companyEmail, setCompanyEmail] = useState(null)
+    const [companyPassword, setCompanyPassword] = useState(null)
+    const [companyName, setCompanyName] = useState(null)
+    const [companyIdentifier, setCompanyIdentifier] = useState(null)
     const [isShowCart, setIsShowCart] = useState(false);
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:5036/Auth/Login', {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ Email: loginEmail, Password: loginPassword })
+            });
+            if (!res.ok) {
+                notification.error({ message: 'Email or password incorrect!' });
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+
+            navigate('/');
+            notification.success({ message: 'Successful login. Welcome!' });
+            setTimeout(function () {
+                window.location.reload();
+            }, 2000);
+        }
+        catch (error) {
+            throw error;
+        }
+    }
     async function handleLogout() {
         try {
             const res = await fetch('http://localhost:5036/Auth/Logout', {
@@ -56,6 +94,52 @@ function Layout({ userRole, user }) {
         }
         catch (error) {
             throw error;
+        }
+    }
+
+    const handleSaveCustomer = async e => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:5036/Auth/Register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ Email: customerEmail, Username: customerUserName, Password: customerPassword })
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                throw new Error(`${data[Object.keys(data)[0]][0]}`);
+            }
+            navigate('/');
+            notification.success({ message: 'Successful registration!' });
+        }
+        catch (error) {
+            notification.error({ message: `Couldn't register. ${error.message}` });
+        }
+    }
+
+    const handleSaveCompany = async e => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:5036/Auth/RegisterCompany', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    Email: companyEmail,
+                    Password: companyPassword,
+                    Username: companyUserName,
+                    CompanyName: companyName,
+                    Identifier: companyIdentifier
+                })
+            });
+            const data = res.json();
+            if (!res.ok) {
+                throw new Error(`${data[Object.keys(data)[0]][0]}`);
+            }
+            navigate('/');
+            notification.success({ message: 'Successful registration!' });
+        }
+        catch (error) {
+            notification.error({ message: `Couldn't register. ${error.message}` });
         }
     }
 
@@ -103,10 +187,10 @@ function Layout({ userRole, user }) {
                                     </> :
                                     <>
                                         <li className="registration-layout">
-                                            <Link className="nav-link" to="/register">Registration</Link>
+                                            <div className="nav-link" id="register-link" onClick={e => setShowRegister(true)}>Registration</div>
                                         </li>
                                         <li className="login-layout">
-                                            <Link className="nav-link" to="/login">Login</Link>
+                                            <Link className="nav-link" onClick={e => setShowLogin(true)}>Login</Link>
                                         </li>
                                     </>
                                 }
@@ -137,6 +221,71 @@ function Layout({ userRole, user }) {
                     </nav>
                 </div>
             </footer>
+            <Modal className='login-modal' dismissible show={showLogin} onClose={() => setShowLogin(false)}>
+                <Modal.Body className='login-modal-body'>
+                    <div className='modal-header'>
+                        <h1>Login</h1>
+                        <button className='close-modal' onClick={e => setShowLogin(false)}>X</button>
+                    </div>
+                    <div>
+                        <h3>Email:</h3>
+                        <input className="login-input" type="text" onChange={e => setLoginEmail(e.target.value)} value={loginEmail}></input>
+                        <h3>Password:</h3>
+                        <input type="password" className="login-input" onChange={e => setLoginPassword(e.target.value)} value={loginPassword}></input>
+                    </div>
+                    <button className="confirm" onClick={e => { handleLogin(e), setShowLogin(false) }}>Log in</button>
+                </Modal.Body>
+            </Modal>
+            <Modal className='register-modal' dismissible show={showRegister} onClose={() => setShowRegister(false)}>
+                <Modal.Body className='register-modal-bigbody'>
+                    <div className='register-modal-header'>
+                        <h1>Register as... </h1>
+                        <button className='close-modal' onClick={e => setShowRegister(false)}>X</button>
+                    </div>
+                    <div className='register-modal-body'>
+                        <button className='choose-customer' onClick={e => { setShowRegisterCustomer(true), setShowRegister(false) }}>a Customer</button>
+                        <button className='choose-company' onClick={e => { setShowRegisterCompany(true), setShowRegister(false) }}>a Company</button>
+                    </div>
+                </Modal.Body>
+            </Modal>
+            <Modal className='login-modal' dismissible show={showRegisterCustomer} onClose={() => setShowRegisterCustomer(false)}>
+                <Modal.Body className='login-modal-body'>
+                    <div className='modal-header'>
+                        <h1>Register</h1>
+                        <button className='close-modal' onClick={e => setShowRegisterCustomer(false)}>X</button>
+                    </div>
+                    <div>
+                        <h3>User name:</h3>
+                        <input className="login-input" type="text" onChange={e => setCustomerUserName(e.target.value)} value={customerUserName}></input>
+                        <h3>Email:</h3>
+                        <input className="login-input" type="text" onChange={e => setCustomerEmail(e.target.value)} value={customerEmail}></input>
+                        <h3>Password:</h3>
+                        <input type="password" className="login-input" onChange={e => setCustomerPassword(e.target.value)} value={customerPassword}></input>
+                    </div>
+                    <button className="confirm" onClick={e => { handleSaveCustomer(e), setShowRegisterCustomer(false) }}>Save</button>
+                </Modal.Body>
+            </Modal>
+            <Modal className='login-modal' dismissible show={showRegisterCompany} onClose={() => setShowRegisterCompany(false)}>
+                <Modal.Body className='login-modal-body'>
+                    <div className='modal-header'>
+                        <h1>Register</h1>
+                        <button className='close-modal' onClick={e => setShowRegisterCompany(false)}>X</button>
+                    </div>
+                    <div>
+                        <h3>User name:</h3>
+                        <input className="login-input" type="text" onChange={e => setCompanyUserName(e.target.value)} value={companyUserName}></input>
+                        <h3>Email:</h3>
+                        <input className="login-input" type="text" onChange={e => setCompanyEmail(e.target.value)} value={companyEmail}></input>
+                        <h3>Password:</h3>
+                        <input type="password" className="login-input" onChange={e => setCompanyPassword(e.target.value)} value={companyPassword}></input>
+                        <h3>Company's name:</h3>
+                        <input className="login-input" type="text" onChange={e => setCompanyName(e.target.value)} value={companyName}></input>
+                        <h3>Identifier:</h3>
+                        <input className="login-input" type="text" onChange={e => setCompanyIdentifier(e.target.value)} value={companyIdentifier}></input>
+                    </div>
+                    <button className="confirm" onClick={e => { handleSaveCompany(e), setShowRegisterCompany(false) }}>Save</button>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
